50cd9fccf04f67a6a4a6be44d49404ef21632287
adrarom <adrarom@gmail.com>
2025-01-25 01:48:08 +0100

    FEAT - Added Character Controller + URP

---
+++ Assets/Scripts/Main_Character/TopDownMovement.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 5f;
+    private float sprintingSpeed; 
+    public float rotationSpeed = 10f;
+    private Vector3 moveDirection;
+    public InputAction movement,sprint;
+    private void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX |
+                         RigidbodyConstraints.FreezeRotationZ |
+                         RigidbodyConstraints.FreezePositionY;
+        sprintingSpeed = speed * 1.25f;
+    }
+
+    private void OnEnable()
+    {
+        movement.Enable();
+        sprint.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+        sprint.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+        float actualSpeed = sprint.IsPressed() ? sprintingSpeed : speed;
+
+        Vector2 input = movement.ReadValue<Vector2>();
+        moveDirection = new Vector3(input.x, 0, input.y).normalized;
+
+        rb.MovePosition(rb.position + moveDirection * actualSpeed * Time.fixedDeltaTime);
+
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime));
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Client") && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            Debug.Log("Interacción con: " + other.name);
+            InteractWithStation(other);
+        }
+    }
+
+    private void InteractWithStation(Collider station)
+    {
+
+        Debug.Log("Ejecutando lógica de interacción para: " + station.name);
+    }
+}
