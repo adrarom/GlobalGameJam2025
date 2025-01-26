9449c30d8d0622aef483c3e6692b7c3d1e6a5441
juches21 <draco0221@gmail.com>
2025-01-26 14:04:23 +0100

    menu 2 el retorno

--- Assets/Juan/Script/Menu.cs
+++ Assets/Juan/Script/Menu.cs
@@ -8,24 +8,15 @@ public class Menu : MonoBehaviour
    public bool estado = false;
     public GameObject panel;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 
     public void jugar()
     {
-        //SceneManager.LoadScene("1");
+        SceneManager.LoadScene("SampleScene");
     }
     public void how()
     {
-        print("sd");
+    
         if (!estado)
         {
             estado = true;
